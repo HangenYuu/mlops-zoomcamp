eaec0019437306de868f54ed1a173504ed81d1fb
Pham Nguyen Hung <97870091+HangenYuu@users.noreply.github.com>
2024-05-27 13:28:40 +0000

    Finished homework 2

--- cohorts/2024/02-experiment-tracking/homework/train.py
+++ cohorts/2024/02-experiment-tracking/homework/train.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
@@ -15,19 +16,22 @@ def load_pickle(filename: str):
 @click.option(
     "--data_path",
     default="./output",
-    help="Location where the processed NYC taxi trip data was saved"
+    help="Location where the processed NYC taxi trip data was saved",
 )
 def run_train(data_path: str):
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        mlflow.set_tag("model", "sklearn.RandomForestRegressor")
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+    return rmse
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_train()
